b29938c51691deadb28cb9cd4b0cd82eaca2e550
Phuoc Huynh <huynhphvan@gmail.com>
2022-04-15 23:17:45 +0700

    fix cors

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const fetch = axios.create({
   baseURL: process.env.REACT_APP_AXIOS_BASE_URL || "http://localhost:8080",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
   withCredentials: true,
 });
 
