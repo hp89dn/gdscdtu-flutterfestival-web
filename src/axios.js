d06c6e358891b53ff55f0eeb7a71b45dfdfb16b3
Phuoc Huynh <huynhphvan@gmail.com>
2022-04-15 23:18:52 +0700

    fix content type

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,9 @@ import axios from "axios";
 const fetch = axios.create({
   baseURL: process.env.REACT_APP_AXIOS_BASE_URL || "http://localhost:8080",
   headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
+		'Access-Control-Allow-Origin': '*',
+		'Content-Type': 'application/json',
+	},
   withCredentials: true,
 });
 
